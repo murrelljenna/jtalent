d5812fc778ad0ce53da7774a83ab38d4a3e7f951
Jenna Murrell <murrelljenna@gmail.com>
2021-12-13 21:29:23 -0500

    Add CORS middleware

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,7 @@
 import RegistrantHttp
 import Web.Scotty
+import Network.Wai.Middleware.Cors
 
-main = scotty 3000 registrantHttp
+main = scotty 3000 $ do
+    middleware simpleCors
+    registrantHttp
